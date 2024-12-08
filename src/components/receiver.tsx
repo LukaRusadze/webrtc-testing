f83ddbf77b1e57eb7c04a4e5631f142aa341471c
LukaRusadze <rusadzeluka@gmail.com>
2024-12-09 02:27:44 +0400

    add some turn server

--- src/components/receiver.tsx
+++ src/components/receiver.tsx
@@ -16,6 +16,11 @@ const pc = new RTCPeerConnection({
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     { urls: "stun:stun1.l.google.com:19302" },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
   ],
   iceCandidatePoolSize: 10,
 });
