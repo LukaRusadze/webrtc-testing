029cc29f062e4f2986827fa27cf959b9dbdb24fc
LukaRusadze <rusadzeluka@gmail.com>
2024-12-09 02:17:26 +0400

    refactor: ice servers

--- src/components/caller.tsx
+++ src/components/caller.tsx
@@ -13,13 +13,8 @@ import { useMemo, useEffect } from "react";
 
 const pc = new RTCPeerConnection({
   iceServers: [
-    {
-      urls: [
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-        "stun:stun4.l.google.com:5349",
-      ],
-    },
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
   ],
   iceCandidatePoolSize: 10,
 });
